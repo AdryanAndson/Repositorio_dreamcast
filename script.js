0a35230980a015b414eb1a30f3700cbb116d79fc
Kapa <121322532+AdryanAndson@users.noreply.github.com>
2024-05-16 16:58:18 -0400

    Add files via upload
    Repository

---
+++ script.js
@@ -0,0 +1,6 @@
+
+    // Função para reproduzir a música quando uma capa de jogo for clicada
+    function playMusic(musicId) {
+        var music = document.getElementById(musicId);
+        music.play();
+    }
